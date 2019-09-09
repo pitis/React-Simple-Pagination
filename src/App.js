7eef2d026f8c0c13f5ecdbc3c39ae63451845cb9
Pitis <pitis.radu@gmail.com>
2019-09-09 22:45:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import Posts from "./components/Posts"
+import Pagination from "./components/Pagination"
+import axios from "axios"
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage, setPostsPerPage] = useState(10)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true)
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      setPosts(res.data)
+      setLoading(false)
+    }
+
+    fetchPosts()
+  }, [postsPerPage])
+
+  //Get current posts
+  const indexOfLastPost = currentPage * postsPerPage
+  const indexOfFirstPost = indexOfLastPost - postsPerPage
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
+
+  //Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
+      <input className="mt-3" id="daNumber" />
+      <button
+        className="mt-3"
+        onClick={() =>
+          setPostsPerPage(document.querySelector("#daNumber").value)
+        }
+      >
+        Change posts per page
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
